1831d1555e2bd2814f8921abb57223ebe9a4f651
Joao Astolfi <jpastolfi@gmail.com>
2023-02-28 15:46:45 +0000

    exercicios m01s06d01

--- modulo 01 - fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/README.md
+++ modulo 01 - fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/README.md
@@ -19,3 +19,6 @@ Crie uma função para mostrar o tamanho de um objeto.
 Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 
 Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves lesson1, lesson2 e lesson3. Ao executar o comando console.log(allLessons), a saída deverá ser a seguinte:
+
+
+>> script2.js
--- modulo 01 - fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/script2.js
+++ modulo 01 - fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/script2.js
@@ -1,3 +1,4 @@
+/* Suponha que você esteja trabalhando em uma escola e precise fazer algumas atualizações no sistema. */
 const lesson1 = {
 	materia: 'Matemática',
 	numeroEstudantes: 20,
@@ -19,22 +20,36 @@ const lesson3 = {
 };
 
 /* Crie uma função para adicionar o turno da noite na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela. */
-const addNightShift = (
-	objetoParaModificar,
-	chaveParaAdicionar,
-	valorDaChave
-) => {};
-
-addNightShift(lesson2, lesson3, 'turno');
-console.log(lesson2);
+const addNightShift = (objetoParaModificar, chaveParaAdicionar, valorDaChave) => objetoParaModificar[chaveParaAdicionar] = valorDaChave;
+addNightShift(lesson2, Object.keys(lesson3)[3], lesson3.turno);
 /* Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro. */
-const displayKeys = (object) => {};
-
+const displayKeys = (object) => console.log(Object.keys(object));
 /* Crie uma função para mostrar o tamanho de um objeto. */
-const displayObjectLength = () => {};
-
+const displayObjectLength = (object) => console.log(Object.entries(object).length);
 /* Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro. */
-const displayValues = (object) => {};
-
+const displayValues = (object) => console.log(Object.values(object));
 /* Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves lesson1, lesson2 e lesson3. */
-const allLessons = {};
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
+/* Usando o objeto criado no tópico anterior, crie uma função que retorne o número total de estudantes em todas as aulas. */
+const totalStudentes = () => {
+	let numeroEstudantesTotal = 0;
+	const allKeys = Object.keys(allLessons);
+	for (let i = 0; i < allKeys.length; i += 1) {
+		numeroEstudantesTotal += allLessons[allKeys[i]].numeroEstudantes;
+	}
+	return numeroEstudantesTotal;
+}
+/* Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo: */
+const getValueByNumber = (object, value) => {
+	const allKeys = Object.keys(object);
+	const chosenKey = allKeys[value];
+	return object[chosenKey];
+}
+/* Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles o objeto, o nome da chave e o valor da chave. Exemplo: */
+const verifyPair = (object, key, value) => {
+	const allKeys = Object.keys(object);
+	const keyLocation = allKeys.indexOf(key);
+	const allValues = Object.values(object);
+	const valueLocation = allValues.indexOf(value);
+	return keyLocation === valueLocation;
+}
